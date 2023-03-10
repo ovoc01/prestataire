012a745c81eedf4aa027722510e0a5201560134e
tendrynyavo <tendrirazafimiandrisoa@gmail.com>
2023-03-10 10:44:03 +0300

    new update

--- controller.rb
+++ controller.rb
@@ -16,6 +16,7 @@ post '/list_prestataire' do
   id_road = params['rn']
   road = Utils.getSpecific_road(id_road,c)
   list = Utils.list_prestataire(id_road,c)
+  puts "tay"
   f = road.formula(c)
   erb :list , :locals => {:prestataire => list,:rn =>road,:formula =>f[0]}
 end
